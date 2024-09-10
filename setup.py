65f45731604ff81051ded127158ea36e041ab1a6
Mamoon Ahmed <engineer.mamoonahmed@gmail.com>
2024-09-11 02:02:27 +0500

    add long description

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     author='Mamoon Ahmed',
     author_email='engineer.mamoonahmed@gmail.com',
     url='https://github.com/MamoonAhmad/identity-reporting/tree/main/identity-trace-python-agent',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         "jsonpickle",
         "requests"
